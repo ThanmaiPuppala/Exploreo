48a5ba0b70be96518f2adec00f391421f206c44e
ThanmaiPuppala <thanmaipuppala@gmail.com>
2025-07-04 18:39:59 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import GetApp from "@/components/GetApp";
 import Hero from "@/components/Hero";
 import Packages from "@/components/Packages";
 import Testimonial from "@/components/Testimonial";
-import Image from "next/image";
+
 
 export default function Home() {
   return (
